86dc28bd1ac571a295306a276b84c8ca0e5c44fd
zimplexing <zhangx_0728@163.com>
2015-10-10 10:46:01 +0800

    新demo

---
+++ demo/scripts/controllers/controllerdemo.js
@@ -0,0 +1,7 @@
+/**
+ * Created by xiangxiao3 on 2015/10/9.
+ */
+var controllerDemoModule = angular.module('controllerDemo', [])
+    .controller('controllerDemoController', function($scope){
+        $scope.controllerName = "World";
+    })
---
+++ demo/scripts/index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by xiangxiao3 on 2015/10/9.
+ */
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'mobile-angular-ui',
+    'controllerDemo'
+]);
+
+myApp.run(function($rootScope){
+    $rootScope.name = "World";
+})
+
+myApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/HelloWorld.html'
+    })
+
+    $routeProvider.when('/controllerdemo', {
+        templateUrl: 'views/controllerdemo.html',
+        controller: 'controllerDemoController'
+    })
+}])
+
+
