a9496969c7f98eea4b54f5fe29dac8bd76a72717
zimplexing <zhangx_0728@163.com>
2015-10-20 09:10:31 +0800

    add new demo by zhangxin

--- demo/scripts/index.js
+++ demo/scripts/index.js
@@ -3,22 +3,36 @@
  */
 var myApp = angular.module('myApp', [
     'ngRoute',
-    'controllerDemo'
+    'controllerDemo',
+    'bindingdemo',
+    'filterdemo',
+    'directivedemo'
 ]);
 
 myApp.run(function($rootScope){
     $rootScope.name = "World";
-})
+});
 
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'views/HelloWorld.html'
-    })
+    });
 
     $routeProvider.when('/controllerdemo', {
         templateUrl: 'views/controllerdemo.html',
         controller: 'controllerDemoController'
+    });
+    $routeProvider.when('/bindingdemo',{
+        templateUrl: 'views/bindingdemo.html',
+        controller: 'bindingdemocontroller'
+    });
+    $routeProvider.when('/filterdemo',{
+        templateUrl: 'views/filterdemo.html',
+        controller: 'filterdemocontroller'
+    });
+    $routeProvider.when('/directivedemo',{
+        templateUrl: 'views/directivedemo.html'
     })
-}])
+}]);
 
 
