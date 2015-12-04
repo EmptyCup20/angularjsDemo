804c6ea4e829560f10b618dc1ab4f0db65326c41
zimplexing <zhangx_0728@163.com>
2015-12-04 16:48:46 +0800

    update some test demo js

--- test/unit/controllers/httpEvent.test.js
+++ test/unit/controllers/httpEvent.test.js
@@ -6,24 +6,63 @@
  */
 'use strict';
 
-describe('fliterControllerTest', function(){
+/*describe('fliterControllerTest', function(){
+ var scope;
+ beforeEach(angular.mock.module('myApp'));
+ beforeEach(angular.mock.inject(function($rootScope, $controller){
+ scope = $rootScope.$new();
+ $controller('personCtrl_1',{$scope:scope});
+ $controller('personCtrl_2',{$scope:scope});
+ }));
+ it('test the scope lastName varible', function(){
+ expect(scope.firstName).toBe("John");
+ });
+ it('test the scope lastName varible', function(){
+ expect(scope.lastName).toBe("Doe");
+ });
+ //it('test the scope lastName varible', function(){
+ //    expect(scope.myVar).toBeFalsy();
+ //});
+ it('test the scope lastName varible', function(){
+ expect(scope.myVar).toBeTruthy();
+ });
+ });*/
+
+describe('fliterControllerTest', function () {
     var scope;
     beforeEach(angular.mock.module('myApp'));
-    beforeEach(angular.mock.inject(function($rootScope, $controller){
-        scope = $rootScope.$new();
-        $controller('personCtrl_1',{$scope:scope});
-        $controller('personCtrl_2',{$scope:scope});
-    }));
-    it('test the scope lastName varible', function(){
-        expect(scope.firstName).toBe("John");
+    describe('personCtrl_1', function () {
+        beforeEach(angular.mock.inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+            $controller('personCtrl_1', {
+                $scope: scope
+            });
+        }));
+        it('test the scope lastName varible', function () {
+            expect(scope.firstName).toBe("John");
+        });
+        it('test the scope lastName varible', function () {
+            expect(scope.lastName).toBe("Doe");
+        });
+        it('test the scope lastName varible', function () {
+            expect(scope.myVar).toBeFalsy();
+        });
     });
-    it('test the scope lastName varible', function(){
-        expect(scope.lastName).toBe("Doe");
-    });
-    //it('test the scope lastName varible', function(){
-    //    expect(scope.myVar).toBeFalsy();
-    //});
-    it('test the scope lastName varible', function(){
-        expect(scope.myVar).toBeTruthy();
+    describe('personCtrl_1', function () {
+        beforeEach(angular.mock.inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+            $controller('personCtrl_2', {
+                $scope: scope
+            });
+        }));
+        it('test the scope lastName varible', function () {
+            expect(scope.firstName).toBe("John");
+        });
+        it('test the scope lastName varible', function () {
+            expect(scope.lastName).toBe("Doe");
+        });
+        it('test the scope lastName varible', function () {
+            expect(scope.myVar).toBeTruthy();
+        });
     });
 });
